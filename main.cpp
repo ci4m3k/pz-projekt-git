e1fd07e00758d9ab3af3ffc45e3ff9cbac1193fc
ci4m3k <mateqpl87@gmail.com>
2022-11-07 18:46:42 +0100

    Dodano nowe funkcje do menu (wer.2.5)

--- main.cpp
+++ main.cpp
@@ -86,15 +86,50 @@ void funkcja_kwadratowa()
     }
 }
 
+void funkcja_kwadratowa_kanoniczna()
+{
+    float a,p,q,x1,x2,delta;
+
+    cout << "Witaj w programie do obliczania miejsc zerowych funkcji kwadratowej!\n" << endl;
+
+    cout << "Podaj a = ";
+    cin >> a;
+
+	cout << "Podaj p = ";
+	cin >> p;
+
+	cout << "Podaj q = ";
+	cin >> q;
+
+	delta = -q/4*a;
+
+    if(delta < 0)
+    {
+        cout << "\nFunkcja nie posiada miejsc zerowych!";
+    }
+    else if (delta == 0)
+    {
+        x1 = p;
+        cout << "\nFunkcja ma jedno miejsce zerowe x = " << x1;
+    }
+    else
+    {
+        x1 = p-sqrt(-q/a);
+        x2 = p+sqrt(-q/a);
+        cout << "\nFunkcja ma dwa miejsca zerowe x1 = " << x1 << ", x2 = " << x2;
+    }
+}
+
 int main()
 {
 	int wybor=0;
 
-	while (wybor>2 || wybor<1)
+	while (wybor>3 || wybor<1)
     {
         cout << "MENU" << endl;
         cout << "1. Funkcja liniowa" << endl;
         cout << "2. Funkcja kwadratowa" << endl;
+        cout << "3. Funkcja kwadratowa - postac kanoniczna" << endl;
 
         cout << "\nWybierz: ";
         cin >> wybor;
@@ -109,6 +144,9 @@ int main()
         break;
     case 2:
         funkcja_kwadratowa();
+        break;
+    case 3:
+        funkcja_kwadratowa_kanoniczna();
         break;
     default:
         break;
